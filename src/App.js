08aac80597a3479bce16d6770f5c6320c3439c22
CingSharped <chrismsharpe123@gmail.com>
2022-06-12 10:21:23 +0100

    exercise props
    fixed prop passing on exercise

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Exercise />
+        <Exercise exerciseName="Pushup" exerciseDescription="Go down, pushup"/>
         <Timer />
       </header>
     </div>
--- src/Exercise.js
+++ src/Exercise.js
@@ -3,16 +3,14 @@ import React from 'react'
 class Exercise extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {exerciseName: "Exercise Name", exerciseDescription: "This is how to perform the exercise"};
-
     }
 
   
     render() {
       return (
         <div className='exerciseDisplay'>
-            <h1>{this.state.exerciseName}</h1>
-            <p>{this.state.exerciseDescription}</p>
+            <h1>{this.props.exerciseName}</h1>
+            <p>{this.props.exerciseDescription}</p>
         </div>
       );
     }
